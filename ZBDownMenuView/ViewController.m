da4c8fdf4440e934740b44019f96df9e5f43c3d4
biyuhuaping <biyuhuaping@163.com>
2023-07-20 15:41:28 +0800

    增加菜单弹框

--- ZBDownMenuView/ViewController.m
+++ ZBDownMenuView/ViewController.m
@@ -8,11 +8,14 @@
 
 #import "ViewController.h"
 #import "ZBDownMenuView.h"
+#import "YCMenuView.h"
 
 @interface ViewController ()<ZBDownMenuViewDelegate>
 @property (strong, nonatomic) IBOutlet ZBDownMenuView *downMenuView;
 @property (strong, nonatomic) NSMutableArray *dataListArray;
 @property (strong, nonatomic) NSMutableArray *allDataArray;
+@property (nonatomic, strong) UIButton *YCMenuBtn;
+
 @end
 
 @implementation ViewController
@@ -20,6 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self conflictingDownMenuView];
+    
+    self.YCMenuBtn.frame = CGRectMake(15, 450, 120, 40);
+    [self.view addSubview:self.YCMenuBtn];
 }
 
 /* 配置DownMenuView */
@@ -44,6 +50,73 @@
  */
 - (void)downMenuView:(ZBDownMenuView *)menuView currentButtonTitle:(NSString *)title andCurrentTitleArray:(NSArray *)currentTitleArray{
     [NSString stringWithFormat:@"您当前选中的是\n(%@)\n 当前所有展示的是\n%@", title, currentTitleArray];
+}
+
+#pragma mark - YCMenuBtn
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self showYCMenuView:nil event:event];
+}
+
+- (void)YCMenuBtnAction:(UIButton *)sender{
+    [self showYCMenuView:sender event:nil];
+}
+
+- (void)showYCMenuView:(UIButton *)sender event:(id)event{
+    CGPoint touchPosition = CGPointZero;
+    CGFloat width = 120;
+    if (sender){//有按钮时，根据按钮位置、宽度展示
+        touchPosition = sender.center;
+        width = MAX(120, CGRectGetWidth(sender.frame));
+    }else if (event){
+        //获取触摸点的集合，可以判断多点触摸事件
+        NSSet *touches = [event allTouches];
+        //两句话是保存触摸起点位置
+        UITouch *touch = [touches anyObject];
+        touchPosition = [touch locationInView:self.view];
+    }
+    
+    NSArray *imageArr = @[@"swap", @"selected", @"code"];
+    NSArray *titleArr = @[@"扫一扫", @"拍    照", @"付款码"];
+    NSMutableArray *mArray = [[NSMutableArray alloc]init];
+    for (int i = 0; i < titleArr.count; i++) {
+        UIImage *img = [UIImage imageNamed:imageArr[i]];
+        YCMenuAction *mAction = [YCMenuAction actionWithTitle:titleArr[i] image:img handler:^(YCMenuAction *action) {
+            NSLog(@"点击了%@",action.title);
+        }];
+        [mArray addObject:mAction];
+    }
+    
+    // 创建
+    YCMenuView *view = [YCMenuView menuWithActions:mArray width:width atPoint:touchPosition];
+    [self.view addSubview:view];
+    
+    // 自定义设置
+    view.menuColor = [UIColor whiteColor];
+    view.isShowShadow = YES;
+    view.separatorColor = UIColor.lightGrayColor;
+//    view.offset = self.isHiddenNav ? kNavBarH + 45 + 15 : 20;
+    view.textColor = UIColor.lightTextColor;
+    view.textFont = [UIFont systemFontOfSize:16];
+    view.menuCellHeight = 44;
+    view.backgroundColor = UIColor.grayColor;
+    [view show];
+}
+
+- (UIButton *)YCMenuBtn{
+    if (!_YCMenuBtn){
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.semanticContentAttribute = UISemanticContentAttributeForceRightToLeft;
+        [btn setImage:[UIImage imageNamed:@"shop_more"] forState:UIControlStateNormal];
+        btn.titleLabel.font = [UIFont systemFontOfSize:16];
+        [btn setTitle:@"YCMenuView" forState:UIControlStateNormal];
+        [btn setTitleColor:UIColor.blueColor forState:UIControlStateNormal];
+        [btn addTarget:self action:@selector(YCMenuBtnAction:) forControlEvents:UIControlEventTouchUpInside];
+        btn.layer.cornerRadius = 2.0;
+        btn.layer.borderWidth = 1.0;
+        btn.layer.borderColor = UIColor.blueColor.CGColor;
+        _YCMenuBtn = btn;
+    }
+    return _YCMenuBtn;
 }
 
 @end
