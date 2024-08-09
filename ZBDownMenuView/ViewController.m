f75dcd557a95adb81af556fe25257a2e77e8f9ff
biyuhuaping <biyuhuaping@163.com>
2024-08-09 19:09:55 +0800

    格式

--- ZBDownMenuView/ViewController.m
+++ ZBDownMenuView/ViewController.m
@@ -34,13 +34,14 @@
     [self.view addSubview:self.collectionMenuBtn];
 }
 
-/* 配置DownMenuView */
+/// 配置DownMenuView
 - (void)conflictingDownMenuView {
     _downMenuView.titleArray = [NSArray arrayWithObjects:@"综合排序",@"价格", @"时间", nil];
-    _downMenuView.dataSourceArr = @[@[@"速度最快", @"评分最高", @"起送价格最低", @"配送费最低", @"人均高到低", @"人均低到高"],
-                                        @[@"0-50元", @"50-100元", @"100-200元", @"200元以上"],
-                                        @[@"1分钟以内", @"1-3分钟", @"3-5分钟", @"5分钟以上"]
-                                        ].mutableCopy;
+    _downMenuView.dataSourceArr = @[
+        @[@"速度最快", @"评分最高", @"起送价格最低", @"配送费最低", @"人均高到低", @"人均低到高"],
+        @[@"0-50元", @"50-100元", @"100-200元", @"200元以上"],
+        @[@"1分钟以内", @"1-3分钟", @"3-5分钟", @"5分钟以上"]
+    ].mutableCopy;
 
     /// 回调方式一: block
     __weak typeof(self) weakSelf = self;
@@ -51,9 +52,7 @@
     };
 }
 
-/**
- *  回调方式二: delegate
- */
+/// 回调方式二: delegate
 - (void)downMenuView:(ZBDownMenuView *)menuView currentButtonTitle:(NSString *)title andCurrentTitleArray:(NSArray *)currentTitleArray{
     [NSString stringWithFormat:@"您当前选中的是\n(%@)\n 当前所有展示的是\n%@", title, currentTitleArray];
 }
